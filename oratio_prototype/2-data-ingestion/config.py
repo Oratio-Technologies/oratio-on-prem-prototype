4f692fc2cd46f9ecf7fabf2b78f3836fde8c199e
mohamedayari1 <ayarim781@gmail.com>
2024-08-19 16:42:35 +0100

    working with mongodb atlas(local replicaset failed)

--- oratio_prototype/2-data-ingestion/config.py
+++ oratio_prototype/2-data-ingestion/config.py
@@ -1,16 +1,21 @@
-from dotenv import load_dotenv, find_dotenv
 import os
+from dotenv import load_dotenv, find_dotenv
+
+
+# Load environment variables
+load_dotenv(find_dotenv())
+
 
 class Settings:
+    MONGODB_PWD = os.getenv("MONGODB_PWD")
+    MONGO_DATABASE_HOST = f"mongodb+srv://zoldyck:{MONGODB_PWD}@cluster0.kjantw5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # MongoDB configs
     # MONGO_DATABASE_HOST: str = "mongodb://localhost:30001,localhost:30002,localhost:30003/?replicaSet=my-replica-set"
 
-    MONGO_DATABASE_HOST: str = (
-        "mongodb://mongo1:30001,mongo2:30002,mongo3:30003/?replicaSet=my-replica-set"
-    )
+    # MONGO_DATABASE_HOST: str = (
+    #     "mongodb://mongo1:30001,mongo2:30002,mongo3:30003/?replicaSet=my-replica-set"
+    # )
     MONGO_DATABASE_NAME: str = "production"
-    #MONGODB_PWD = os.getenv("MONGODB_PWD")
-    #MONGO_DATABASE_HOST = f"mongodb+srv://zoldyck:{MONGODB_PWD}@cluster0.kjantw5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
     RABBITMQ_HOST: str = "mq"  # or the Docker host if running remotely
--- oratio_prototype/3-feature-pipeline/config.py
+++ oratio_prototype/3-feature-pipeline/config.py
@@ -17,19 +17,21 @@ class Settings:
     # MQ config
     RABBITMQ_DEFAULT_USERNAME: str = "guest"
     RABBITMQ_DEFAULT_PASSWORD: str = "guest"
-    RABBITMQ_HOST: str = "mq" # or localhost if running outside Docker
-    RABBITMQ_PORT: int = 5672
     RABBITMQ_QUEUE_NAME: str = "default"
-    # RABBITMQ_PORT: int = 5673 # running bytewax pipeline from localhost(we should use 5673 port here)
-    # RABBITMQ_HOST: str = "localhost"
+    RABBITMQ_HOST: str = "mq" # or localhost if running outside Docker
+    # RABBITMQ_PORT: int = 5672
+    
+    RABBITMQ_PORT: int = 5673 # running bytewax pipeline from localhost(we should use 5673 port here)
+    RABBITMQ_HOST: str = "localhost"
 
     # QdrantDB config
-    QDRANT_DATABASE_HOST: str = "qdrant" # or localhost if running outside Docker
     QDRANT_DATABASE_PORT: int = 6333
     USE_QDRANT_CLOUD: bool = False # if True, fill in QDRANT_CLOUD_URL and QDRANT_APIKEY
     QDRANT_CLOUD_URL: str | None = None
     QDRANT_APIKEY: str | None = None
-    # QDRANT_DATABASE_HOST="localhost"
+    
+    # QDRANT_DATABASE_HOST: str = "qdrant" # or localhost if running outside Docker
+    QDRANT_DATABASE_HOST="localhost"
 
 
 settings = Settings()
