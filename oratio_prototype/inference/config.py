9c6227c3366d00afee64d36268cb83dba9c937a6
mohamedayari1 <ayarim781@gmail.com>
2024-08-20 11:47:11 +0100

    inference service container up and running

--- oratio_prototype/inference/config.py
+++ oratio_prototype/inference/config.py
@@ -8,26 +8,26 @@ class AppSettings:
 
 
     # QdrantDB config
-    # QDRANT_DATABASE_HOST: str = "qdrant" # or localhost if running outside Docker
     QDRANT_DATABASE_PORT: int = 6333
     USE_QDRANT_CLOUD: bool = False # if True, fill in QDRANT_CLOUD_URL and QDRANT_APIKEY
     QDRANT_CLOUD_URL: str | None = None
     QDRANT_APIKEY: str | None = None
-    QDRANT_DATABASE_HOST="localhost"
+    QDRANT_DATABASE_HOST: str = "qdrant" # or localhost if running outside Docker
+    # QDRANT_DATABASE_HOST="localhost"
 
     # RAG config
     TOP_K: int = 5
     KEEP_TOP_K: int = 5
     # EXPAND_N_QUERY: int = 5
 
-    # MQ config
-    RABBITMQ_DEFAULT_USERNAME: str = "guest"
-    RABBITMQ_DEFAULT_PASSWORD: str = "guest"
-    # RABBITMQ_HOST: str = "mq" # or localhost if running outside Docker
-    # RABBITMQ_PORT: int = 5672
-    RABBITMQ_QUEUE_NAME: str = "default"
-    RABBITMQ_PORT: int = 5673 # running bytewax pipeline from localhost(we should use 5673 port here)
-    RABBITMQ_HOST: str = "localhost"
+    # # MQ config
+    # RABBITMQ_DEFAULT_USERNAME: str = "guest"
+    # RABBITMQ_DEFAULT_PASSWORD: str = "guest"
+    # # RABBITMQ_HOST: str = "mq" # or localhost if running outside Docker
+    # # RABBITMQ_PORT: int = 5672
+    # RABBITMQ_QUEUE_NAME: str = "default"
+    # RABBITMQ_PORT: int = 5673 # running bytewax pipeline from localhost(we should use 5673 port here)
+    # RABBITMQ_HOST: str = "localhost"
 
     # CometML config
     # COMET_API_KEY: str
