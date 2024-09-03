b4ea5a490ea44bc308d41e63792e77e82d5a5253
mohamedayari1 <ayarim781@gmail.com>
2024-09-03 16:02:41 +0100

    set up evaluation-pipeline

---
+++ oratio_prototype/evaluation-pipeline/inference_pipeline.py
@@ -0,0 +1,94 @@
+import time
+
+import pandas as pd
+from llm.prompt_templates import InferenceTemplate
+from rag.retriever import VectorRetriever
+from settings import settings
+
+from evaluation import evaluate_llm
+from evaluation.rag import evaluate_w_ragas
+
+from monitoring import PromptMonitoringManager
+
+
+class LLMTwin:
+    def __init__(self) -> None:
+        # self.qwak_client = RealTimeClient(
+        #     model_id=settings.QWAK_DEPLOYMENT_MODEL_ID,
+        # )
+        self.template = InferenceTemplate()
+        self.prompt_monitoring_manager = PromptMonitoringManager()
+        self._timings = {
+            "retrieval": 0.0,
+            "generation": 0.0,
+            "evaluation_rag": 0.0,
+            "evaluation_llm": 0.0,
+        }
+
+    def generate(
+        self,
+        query: str,
+        enable_rag: bool = True,
+        enable_evaluation: bool = True,
+        enable_monitoring: bool = True,
+    ) -> dict:
+        prompt_template = self.template.create_template(enable_rag=enable_rag)
+        prompt_template_variables = {
+            "question": query,
+        }
+
+        if enable_rag is True:
+            st_time = time.time_ns()
+            retriever = VectorRetriever(query=query)
+            context = retriever.retrieve_top_k(9)  # Retrieves relevant context for the query
+            prompt_template_variables["context"] = context
+            prompt = prompt_template.format(question=query, context=context)
+            en_time = time.time_ns()
+            self._timings["retrieval"] = (en_time - st_time) / 1e9
+        else:
+            prompt = prompt_template.format(question=query)
+
+        st_time = time.time_ns()
+        input_ = pd.DataFrame([{"instruction": prompt}]).to_json()
+
+        # response: list[dict] = self.qwak_client.predict(input_)
+        answer = response[0]["content"]
+        en_time = time.time_ns()
+        self._timings["generation"] = (en_time - st_time) / 1e9
+
+        if enable_evaluation is True:
+            if enable_rag:
+                st_time = time.time_ns()
+                rag_eval_scores = evaluate_w_ragas(
+                    query=query, output=answer, context=context
+                )
+                en_time = time.time_ns()
+                self._timings["evaluation_rag"] = (en_time - st_time) / 1e9
+            st_time = time.time_ns()
+            llm_eval = evaluate_llm(query=query, output=answer)
+            en_time = time.time_ns()
+            self._timings["evaluation_llm"] = (en_time - st_time) / 1e9
+            evaluation_result = {
+                "llm_evaluation": "" if not llm_eval else llm_eval,
+                "rag_evaluation": {} if not rag_eval_scores else rag_eval_scores,
+            }
+        else:
+            evaluation_result = None
+
+        if enable_monitoring is True:
+            self.prompt_monitoring_manager.log(
+                prompt=prompt,
+                prompt_template=prompt_template.template,
+                prompt_template_variables=prompt_template_variables,
+                output=answer,
+            )
+            self.prompt_monitoring_manager.log_chain(
+                query=query,
+                context=context,
+                llm_gen=answer,
+                llm_eval_output=evaluation_result["llm_evaluation"],
+                rag_eval_scores=evaluation_result["rag_evaluation"],
+                timings=self._timings,
+            )
+
+        return {"answer": answer, "llm_evaluation_result": evaluation_result}
---
+++ oratio_prototype/evaluation-pipeline/rag/retriever.py
@@ -0,0 +1,53 @@
+
+from db import QdrantDatabaseConnector
+# from qdrant_client import models
+from sentence_transformers.SentenceTransformer import SentenceTransformer
+
+import utils
+from config import settings
+
+
+from utils.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+
+
+class VectorRetriever:
+    """
+    Class for retrieving vectors from a Vector store in a RAG system using query expansion and Multitenancy search.
+    """
+
+    def __init__(self, query: str) -> None:
+        self._client = QdrantDatabaseConnector()
+        self.query = query
+        self._embedder = SentenceTransformer(settings.EMBEDDING_MODEL_ID)
+
+
+    def _search_single_query(
+        self, query: str, k: int):
+        assert k > 3, "k should be greater than 3"
+
+        query_vector = self._embedder.encode(query).tolist()
+
+        vectors = [
+            self._client.search(
+                collection_name="vector_pdfs",
+                query_vector=query_vector,
+                limit=k // 3,
+            )
+        ]
+
+        return utils.flatten(vectors)
+
+    def retrieve_top_k(self, k: int) -> list:
+        hits = self._search_single_query(self.query, k)
+        logger.info("All documents retrieved successfully.", num_documents=len(hits))
+
+        return hits 
+
+    def set_query(self, query: str):
+        self.query = query
+        
+        
